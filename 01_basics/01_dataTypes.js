78856f6a1b648c7a8eb1251f1e3c77fe66e9b842
ChaudhariHansraj <143324653+ChaudhariHansraj@users.noreply.github.com>
2025-04-27 12:55:32 +0000

    Conversion of data types

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,26 @@
+// let number = 10
+// console.log(number);
+// let newNumber = String(number);
+// console.log(newNumber)
+// console.log(typeof newNumber)
+
+// let value = "hansraj"
+// console.log(value)
+// console.log(typeof value)
+
+// let valueInNumber = Number(value)
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+
+let value = ""
+console.log(value);
+console.log(typeof value);
+
+let valueInNumber =Boolean(value)
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+
+
+
+
