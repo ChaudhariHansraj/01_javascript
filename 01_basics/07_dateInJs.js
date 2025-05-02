1385d751c84bd848acabe9fa289066cfcab6dc57
ChaudhariHansraj <143324653+ChaudhariHansraj@users.noreply.github.com>
2025-05-02 17:56:05 +0000

    viwed date and time

---
+++ 01_basics/07_dateInJs.js
@@ -0,0 +1,27 @@
+const date = new Date();
+console.log(date);
+console.log(date.toDateString());
+console.log(date.toLocaleString());
+console.log(date.getDate());
+console.log(date.getDay());
+console.log(date.getFullYear());
+console.log(date.getHours());
+console.log(date.getMilliseconds());
+console.log(date.getMinutes());
+console.log(date.getTime());
+console.log(date.getUTCDate());
+console.log(date.getUTCDay());
+console.log(date.toUTCString());
+console.log(date.toTimeString());
+console.log(date.setDate(3));
+console.log(date.getDate());
+
+console.log(date.getUTCDate());
+console.log(date);
+console.log(date.toTimeString());
+console.log(date.toJSON());
+console.log(date.toISOString());
+console.log(date.toString());
+console.log(date.toLocaleDateString());
+console.log(date.toLocaleString());
+console.log(date.toTimeString());
